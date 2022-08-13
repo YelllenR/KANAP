2e799abbbfa0d537e49317a76465090892940538
YelllenR <aurelie@rosedarbon.com>
2022-08-13 19:16:34 +0200

    regex
    Testing regex on input fields in form

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,13 +1,15 @@
 // Class of produit
 FetchApiAndRenderElements('http://localhost:3000/api/products');
 
+
+
 //  Fetch the API
 function FetchApiAndRenderElements(connextionToApi) {
     fetch(connextionToApi)
         .then(response => response.json())
         .then(jsonResponse => RenderSelectedItemsOnHtml(jsonResponse))
         .then(() => DeletionOfItems())
-        .then(() => CheckingOfInputsInForm())
+        .then(() => ListenOnInputsInForm())
 
 }
 
@@ -22,7 +24,6 @@ function RenderSelectedItemsOnHtml(productList) {
         for (let j = 0; j < productList.length; j++) {
 
             if (cart[i].id === productList[j]._id) {
-                console.log("id = " + cart[i].id);
 
                 let quantity = cart[i].quantity;
                 let image = productList[j].imageUrl;
@@ -159,62 +160,106 @@ function CalculateTotal(productList) {
     }
 }
 
-function CheckingOfInputsInForm() {
-    document.querySelector("form").setAttribute("id", "formPurchase");
-    let form = document.getElementById("formPurchase");
-    //  .addEventListener("submit", function(formInputData){
+GetFormById();
+function GetFormById() {
+    let idTag = (id) => document.getElementById(id);
 
-    // formInputData.preventDefault()
-    // let formOnCart = formInputData.target;
-    //     let formData = new FormData(form);
+    form = idTag("formPurchase"),
+        firstName = idTag("firstName"),
+        lastName = idTag("lastName"),
+        address = idTag("address"),
+        city = idTag("city"),
+        email = idTag("email");
 
-
-    //     let firstName = formData.get("firstName");
-    //     let lastName = formData.get("lastName");
-    //     let address = formData.get("address");
-    //     let city = formData.get("city");
-    //     let email = formData.get("email");
-
-    //     for(let key of formData.key()){
-    //         console.log(key, formData.get(key))
-    //     }
-    // // });
-
+    firstName.required = false;
+    lastName.required = false;
+    address.required = false;
+    city.required = false;
+    email.required = false;
 }
 
-// function IsValid() {
-//     IsValid.preventDefault();
+GetIdErrorMessage();
+function GetIdErrorMessage() {
+    let error = (errorMessage) => document.getElementById(errorMessage);
+    errorLastName = error("lastNameErrorMsg"),
+        errorFirstName = error("firstNameErrorMsg"),
+        errorEmail = error("emailErrorMsg"),
+        errorAddress = error("addressErrorMsg"),
+        errorCity = error("cityErrorMsg");
+}
 
 
-//     for (let value of formData) {
-//         if (firstName[value] == "") {
-//             errorFirstName.innerText = "Merci de remplir ce champ avec votre nom";
-//         }else{
-//             errorFirstName.innerText = "";
-//         }
 
-//         RenderErrorMessage(messageError) 
-//     }
-// }
+function TestRegexOfInputs() {
+    GetFormById();
+
+    let emailRegex = new RegExp("^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$", "g");
+    let testEmail = emailRegex.test(email.value);
+    if (testEmail) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+
+
+
+    let letterOnlyRegex = new RegExp ("^[a-zA-Z][A-zÀ-ú]+$","g");
+
+    let testStringOnly = letterOnlyRegex.test(firstName.value);
+    if (testStringOnly) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+
+
+
+    let addressRegex = new RegExp("/d{1,3}.?d{0,3}s[a-zA-Z]{2,30}s[a-zA-Z]{2,15}", "g");
+
+    
+    let cityRegex = /^[a-zA-Z]+(?:[\s-][a-zA-Z][A-zÀ-ú]+)*/g;
+}
+
+
+function ListenOnInputsInForm() {
+    document.querySelector("form").setAttribute("id", "formPurchase");
+    GetFormById();
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        TestRegexOfInputs()
+        CheckingOfInputsInForm();
+
+        // console.log("Form submited");
+    });
+}
+
+
+function CheckingOfInputsInForm() {
+    GetFormById();
+    GetIdErrorMessage();
+
+    let messageForEmptyFields = "Veuillez renseigner ce champs";
+    let regexErrorMessage = "Veuillez respecter la saisie des champs";
+
+
+
+
+    // if (firstName.value.trim() == letterOnlyRegex || firstName.value.trim() === "") {
+    //     errorFirstName.innerHTML = regexErrorMessage;
+
+    // } else {
+    //     errorFirstName.innerHTML = "yes";
+    //     console.log(errorFirstName.innerHTML = "yes")
+    // }
+}
+
+
 
 function SendingFormData() {
-
+    // form data to send informations
 }
 
-function RenderErrorMessage(messageError) {
-    const errorLastName = document.getElementById("lastNameErrorMsg");
-    const errorFirstName = document.getElementById("firstNameErrorMsg");
-    const errorEmail = document.getElementById("emailErrorMsg");
-    const errorAddress = document.getElementById("addressErrorMsg");
-    const errorCity = document.getElementById("cityErrorMsg");
 
 
-
-    //     errorLastName.innerText = "Merci de remplir ce champ avec votre nom",
-    //     errorFirstName.innerText = "Merci de remplir ce champ avec votre prénom",
-    //     errorEmail.innerText = "Veuillez mettre une adresse email valide",
-    //     errorAddress.innerText = "Veuillez indiquer votre adresse",
-    //     errorCity.innerText = "Veuillez indiquer la ville",
-
-}
 
--- front/js/class.js
+++ front/js/class.js
@@ -18,3 +18,4 @@ class ProducInCart {
         this.quantity = quantity
     }
 }
+
